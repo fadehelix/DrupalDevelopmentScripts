ee9fac90c4fe246ee8ed5c1a5fb4161dfc391968
Adam Dziendziel <fadehelix@gmail.com>
2012-04-10 13:10:24 +0200

    new-drupal-site.sh has been added

---
+++ new-drupal-site.sh
@@ -0,0 +1,60 @@
+#Pobierz nazwe tworzonego vhosta i jednoczesnie usera
+echo  -n "Podaj nazwe tworzonego sajta: "
+read sitename
+echo -n "Podaj domene np. .local (wraz z kropka): "
+read domain
+
+#utworz katalog vhosta
+mkdir -p /var/www/$sitename$domain/public_html
+
+#utworz katalog www
+
+
+
+#zmien ownera
+chown -R adas:www-data /var/www/$sitename$domain/public_html
+
+#Utworz katalog z logami apacha dla vhosta
+mkdir /var/log/apache2/$sitename$domain/
+
+
+#utworzenie konfiguracji vhosta
+echo "<VirtualHost *:80>
+ DocumentRoot /var/www/$sitename$domain/public_html/
+ ServerName $sitename$domain
+ ServerAlias $sitename$domain www.$sitename$domain
+ <Directory /var/www/$sitename$domain/public_html/ >
+  allow from all
+  Options +Indexes FollowSymLinks
+ </Directory>
+ 
+ #plik z logami
+ ErrorLog /var/log/apache2/$sitename$domain/error.log
+ 
+ LogLevel warn
+ 
+ CustomLog /var/log/apache2/$sitename$domain/access.log combined
+ 
+</VirtualHost>" > /etc/apache2/sites-available/$sitename$domain
+
+#utworz dowiazanie
+#ln -s "/etc/apache2/sites-available/$sitename /etc/apache2/sites-enabled/
+
+echo  127.0.0.1    $sitename$domain >>/etc/hosts
+
+a2ensite $sitename$domain
+
+/etc/init.d/apache2 restart
+
+#database
+echo -n: "Uzytkownik bazy danych: "
+read dbuser
+echo -n "Haslo uzytkownika bazy danych: "
+read dbuserpass
+
+mysql -u$dbuser -p$dbuserpass -e "create database $sitename;"
+
+
+#test vhosta
+echo "<h1>Propsy! Utworzyles vhosta <span style='color:red'> ${sitename}${domain} </span> !!!</h1>" > /var/www/$sitename$domain/public_html/index.php
+
